2d5195c3a74fc29af9ef10fb6142ed909ab102cc
Wenjie Zhang <2065882830@qq.com>
2023-03-26 21:42:01 +0800

    Add the setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="dflow_thermal-conductivity",
+    version="0.0.1",
+    author="Wenjie Zhang",
+    author_email="gdbhcxhmjk@163.com",
+    description="A framework for thermal conductivity calculation based on EMD&NEMD methods",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gdbhcxhmjk-z/ThermalConductivity-Workflow.git",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "pydflow>=1.6.27",
+        "lbg>=1.2.13",
+        "dpdata>=0.2.7",
+        "matplotlib",
+        "sportran",
+        "numpy",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+    provides=["dflowTC"]
+)
